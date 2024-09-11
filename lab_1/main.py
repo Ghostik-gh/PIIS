dd5e92d1b90fb3fef74ce7c8ff959290c970fefe
fmashirin <fmashirin@ozon.ru>
2024-09-11 12:44:01 +0300

    solve first and start second

--- lab_1/main.py
+++ lab_1/main.py
@@ -21,24 +21,103 @@ encoder.fit(Y)
 encoded_Y = encoder.transform(Y)
 dummy_y = to_categorical(encoded_Y)
 
-model = Sequential()
+model_1 = Sequential()
+model_1.add(Dense(4, activation="relu"))
+model_1.add(Dense(3, activation="softmax"))
+model_1.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+history_1 = model_1.fit(X, dummy_y, epochs=75, batch_size=10, validation_split=0.1)
 
-model.add(Dense(4, activation="relu"))
-model.add(Dense(3, activation="softmax"))
+model_2 = Sequential()
+model_2.add(Dense(16, activation="relu"))
+model_2.add(Dense(3, activation="softmax"))
+model_2.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+history_2 = model_2.fit(X, dummy_y, epochs=75, batch_size=10, validation_split=0.1)
 
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-history = model.fit(X, dummy_y, epochs=75, batch_size=10, validation_split=0.1)
+model_3 = Sequential()
+model_3.add(Dense(64, activation='relu'))
+model_3.add(Dense(3, activation='softmax'))
+model_3.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+history_3 = model_3.fit(X, dummy_y, epochs=75, batch_size=10, validation_split=0.1)
 
-plt.plot(history.history['loss'], label='loss')
-plt.plot(history.history['val_loss'], label='val_loss')
-plt.plot(history.history['accuracy'], label='accuracy:')
-plt.plot(history.history['val_accuracy'], label='val_accuracy')
-plt.title('График ошибок и точности обучения')
-plt.ylabel('loss & accuracy')
-plt.xlabel('epoch')
-plt.legend(loc="upper left")
+model_4 = Sequential()
+model_4.add(Dense(4, activation='relu'))
+model_4.add(Dense(8, activation='relu'))
+model_4.add(Dense(16, activation='relu'))
+model_4.add(Dense(3, activation='softmax'))
+model_4.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+history_4 = model_4.fit(X, dummy_y, epochs=75, batch_size=10, validation_split=0.1)
+
+model_5 = Sequential()
+model_5.add(Dense(4, activation="relu"))
+model_5.add(Dense(16, activation="linear"))
+model_5.add(Dense(3, activation="softmax"))
+model_5.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+history_5 = model_5.fit(X, dummy_y, epochs=75, batch_size=10, validation_split=0.1)
+
+model_6 = Sequential()
+model_6.add(Dense(4, activation='relu'))
+model_6.add(Dense(9, activation='softmax'))
+model_6.add(Dense(3, activation='softmax'))
+model_6.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+history_6 = model_6.fit(X, dummy_y, epochs=75, batch_size=10, validation_split=0.1)
+
+plt.figure(figsize=(16, 4))
+plt.subplot(2, 3, 1)
+
+plt.title('relu 4, softmax 3')
+plt.plot(history_1.history['loss'], label='Loss', color='red')
+plt.plot(history_1.history['val_loss'], label='Val_loss', color='blue')
+plt.plot(history_1.history['accuracy'], label='Accuracy', color='green')
+plt.plot(history_1.history['val_accuracy'], label='Val_accuracy', color='orange')
+plt.legend()
+
+plt.subplot(2, 3, 2)
+
+plt.title('relu 16, softmax 3')
+plt.plot(history_2.history['loss'], label='Loss', color='red')
+plt.plot(history_2.history['val_loss'], label='Val_loss', color='blue')
+plt.plot(history_2.history['accuracy'], label='Accuracy', color='green')
+plt.plot(history_2.history['val_accuracy'], label='Val_accuracy', color='orange')
+plt.legend()
+
+plt.subplot(2, 3, 3)
+
+plt.title('relu 64, softmax 3')
+plt.plot(history_3.history['loss'], label='Loss', color='red')
+plt.plot(history_3.history['val_loss'], label='Val_loss', color='blue')
+plt.plot(history_3.history['accuracy'], label='Accuracy', color='green')
+plt.plot(history_3.history['val_accuracy'], label='Val_accuracy', color='orange')
+plt.legend()
+
+plt.subplot(2, 3, 4)
+
+plt.title('relu 4, relu 8, relu 16, softmax 3')
+plt.plot(history_4.history['loss'], label='Loss', color='red')
+plt.plot(history_4.history['val_loss'], label='Val_loss', color='blue')
+plt.plot(history_4.history['accuracy'], label='Accuracy', color='green')
+plt.plot(history_4.history['val_accuracy'], label='Val_accuracy', color='orange')
+plt.legend()
+
+plt.subplot(2, 3, 5)
+
+plt.title('relu 4, linear 16, softmax 3')
+plt.plot(history_5.history['loss'], label='Loss', color='red')
+plt.plot(history_5.history['val_loss'], label='Val_loss', color='blue')
+plt.plot(history_5.history['accuracy'], label='Accuracy', color='green')
+plt.plot(history_5.history['val_accuracy'], label='Val_accuracy', color='orange')
+plt.legend()
+
+plt.subplot(2, 3, 6)
+
+plt.title('relu 4, softmax 9, softmax 3')
+plt.plot(history_6.history['loss'], label='Loss', color='red')
+plt.plot(history_6.history['val_loss'], label='Val_loss', color='blue')
+plt.plot(history_6.history['accuracy'], label='Accuracy', color='green')
+plt.plot(history_6.history['val_accuracy'], label='Val_accuracy', color='orange')
+plt.legend()
+
+
 plt.show()
-
 
 
 """
